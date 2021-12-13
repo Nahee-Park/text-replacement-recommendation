a0ace1ed0aae3ae41d2d94b5792ecdea7115f029
nahee-park <jessica396@ewhain.net>
2021-12-14 02:56:09 +0900

    feat: Button 컴포넌트 제작

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from 'react';
 import styled from '@emotion/styled';
+export type sizeType = 'sm' | 'md' | 'lg';
 
-function Button() {
-    return (
-        <button>search</button>
-    )
+interface ButtonStyleProps {
+  color: string;
+  size: sizeType;
+}
+interface ButtonProps extends ButtonStyleProps {
+  label: string;
+}
+
+function Button({ label, color, size }: ButtonProps) {
+  return (
+    <Styled.Button color={color} size={size}>
+      {label}
+    </Styled.Button>
+  );
 }
 
 export default Button;
 
+const Styled = {
+  Button: styled.button<ButtonStyleProps>`
+    border-radius: 12px;
+    background: ${({ color }) => color};
+    ${({ size }) =>
+      'sm'
+        ? 'width: 90px; height: 35px;'
+        : 'md'
+        ? 'width: 106px; height: 45px;'
+        : 'width: 130px; height: 55px;'}
+    color: #FFFFFF;
+    size: 21px;
+    font-family: Roboto Mono Light for Powerline;
+    line-height: 25px;
+    cursor: pointer;
+    &:hover {
+      filter: brightness(115%);
+    }
+  `,
+};
